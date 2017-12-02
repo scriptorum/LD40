0760a14d67b63701829ac07ad4cc4a6bad207979
Eric Lund <github@egotron.com>
2017-12-02 15:52:28 -0500

    Added timer

---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Spewnity;
+using TMPro;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+	public static TimerUI instance;
+	public TextMeshPro text;
+	public float time;
+
+	void Awake()
+	{
+		text.ThrowIfNull();
+		time = 30.0f;
+	}
+
+	void Update()
+	{
+		time -= Time.deltaTime;
+		if (time < 0) time = 0;
+		text.text = System.String.Format("{0:F2}", time);
+	}
+}
