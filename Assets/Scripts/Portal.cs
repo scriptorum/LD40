867f67de9179a82ad5a51365318993e4563ed666
Eric Lund <github@egotron.com>
2017-12-02 16:38:08 -0500

    Added portal - fix

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Spewnity;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+	public TweenManager tm;
+	public CircleCollider2D col;
+
+	void Awake()
+	{
+		tm.ThrowIfNull();
+		gameObject.SelfAssign(ref col);
+	}
+
+	void Start()
+	{
+		Appear();
+	}
+
+	public void Appear()
+	{
+		col.enabled = true;
+		tm.Play("appear");
+		SoundManager.instance.Play("portal");
+	}
+
+	public void Disappear()
+	{
+		col.enabled = false;
+		tm.Play("disappear");
+
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag != "Player")
+			return;
+
+		Debug.Log("Player entered the portal!");
+
+		SoundManager.instance.Play("portal");
+		Disappear();
+	}
+}
