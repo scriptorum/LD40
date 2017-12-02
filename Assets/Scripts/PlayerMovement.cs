36937a0475f47aab11dd7436f9e51e0a7c3fab0b
Eric Lund <github@egotron.com>
2017-12-02 11:41:17 -0500

    Jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,39 +6,56 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private const float MAX_SPEED = 5f;
-    private float speed = 100f;
+	public Transform groundCheck;
 	private Rigidbody2D rb;
 	private SpriteRenderer sr;
+	private const float maxSpeed = 5f;
+	private float walkForce = 200f;
+	private float floatMultiplier = 0.2f;
+	private float jumpForce = 400f;
+	private bool onGround = false;
+	private bool jumping = false;
 
-	void Awake()
+    void Awake()
 	{
 		gameObject.SelfAssign(ref rb);
 		gameObject.SelfAssign(ref sr);
+		groundCheck.ThrowIfNull();
 	}
 
-	// Use this for initialization
 	void Update()
 	{
-		float x = Input.GetAxis("Horizontal");
-		float y = Input.GetAxis("Vertical");
-
-		rb.AddForce(Vector2.right * x * speed);
-		if(Mathf.Abs(rb.velocity.x) > MAX_SPEED) 
-			rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * MAX_SPEED, rb.velocity.y);
-
-		if((sr.flipX && x > 0) || (!sr.flipX && x < 0))
-			sr.flipX = !sr.flipX;
-
-		if(Input.GetKey(KeyCode.R))
-			Reset();
-
-
+		jumping = Input.GetKey(KeyCode.Space);
+		onGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 	}
 
-    private void Reset()
-    {
+	void FixedUpdate()
+	{
+		float x = Input.GetAxis("Horizontal");
+
+		if (onGround && jumping)
+		{
+			rb.AddForce(new Vector2(0f, jumpForce));
+			onGround = false;
+		}
+
+	
+		float curForce = onGround ? walkForce : walkForce * floatMultiplier;
+		rb.AddForce(Vector2.right * x * curForce);
+
+		if (Mathf.Abs(rb.velocity.x) > maxSpeed)
+			rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+
+		if ((sr.flipX && x > 0) || (!sr.flipX && x < 0))
+			sr.flipX = !sr.flipX;
+
+		if (Input.GetKey(KeyCode.R))
+			Reset();
+	}
+
+	private void Reset()
+	{
 		rb.velocity = Vector3.zero;
-        transform.localPosition = new Vector3(0,3,0);
-    }
+		transform.localPosition = new Vector3(0, 3, 0);
+	}
 }
