e01661cb63a024c9a4c22d2e6fabf4873c041cdb
Eric Lund <github@egotron.com>
2017-12-03 16:15:52 -0500

    Tweaking movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,12 @@ public class PlayerMovement : MonoBehaviour
 	public Transform groundCheck;
 	private Rigidbody2D rb;
 	private SpriteRenderer sr;
-	private const float WEIGHT_PENALTY = 18; // Higher numbers is more forgiving, do not go below max inventory size
+	private const float WEIGHT_PENALTY = 20f; // Higher numbers is more forgiving, do not go below max inventory size
+	private const float WEIGHT_PENALTY_TO_MAX_SPEED = 0.4f;
 	private const float maxSpeed = 5f;
-	private float walkForce = 250f;
-	private float floatForce = 100f;
-	private float jumpForce = 600f;
+	private float walkForce = 300f;
+	private float floatForce = 50f;
+	private float jumpForce = 525f;
 	private float lastOnGround = 0;
 	private bool jumping = false;
 
@@ -34,24 +35,25 @@ public class PlayerMovement : MonoBehaviour
 	void FixedUpdate()
 	{
 		float x = Input.GetAxis("Horizontal");
+		int weight = Inventory.instance.GetGoldWeight();
 
 		bool onGround = (Time.realtimeSinceStartup - lastOnGround) < 0.1f;
 		if (onGround && jumping && rb.velocity.y < 0.025f)
 		{
 			rb.velocity = new Vector3(rb.velocity.x, 0f);
-            int weight = Inventory.instance.GetGoldWeight();
-            float actualJumpForce = jumpForce * (1 - weight / WEIGHT_PENALTY);
+            float actualJumpForce = jumpForce - weight * WEIGHT_PENALTY;
 			rb.AddForce(new Vector2(0f, actualJumpForce));
 			SoundManager.instance.PlayAs("jump", 1.2f - weight * 0.1f, 0.8f);
 			onGround = false;
 		}
 
 	
-		float curForce = onGround ? walkForce : floatForce;
+
+		float curForce = onGround ? walkForce - weight * WEIGHT_PENALTY : floatForce;
 		rb.AddForce(Vector2.right * x * curForce);
 
 		if (Mathf.Abs(rb.velocity.x) > maxSpeed)
-			rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+			rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed - weight * WEIGHT_PENALTY_TO_MAX_SPEED, rb.velocity.y);
 
 		if ((sr.flipX && x > 0) || (!sr.flipX && x < 0))
 			sr.flipX = !sr.flipX;
